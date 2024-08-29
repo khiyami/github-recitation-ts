af0e48d660b7f4bb39427a2fce51d656565a0086
Procos12 <64953008+Procos12@users.noreply.github.com>
2024-08-29 17:25:41 +0300

    Add files via upload

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,9 +1,14 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci from "./fib";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const { num } = req.query;  // req.params doesn't exist in Next.js, use req.query instead
+
+  if (typeof num !== 'string' || isNaN(parseInt(num))) {
+    return res.status(400).send('Invalid input');
+  }
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
