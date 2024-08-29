ff17b1f45241d6eff8e2618481bbe732df7e21d5
Procos12 <64953008+Procos12@users.noreply.github.com>
2024-08-29 17:47:19 +0300

    trying to fix the issue

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,17 @@
-
+// Endpoint for querying the fibonacci numbers
 import fibonacci from "./fib";
 import { Request, Response } from "express";
+
 export default (req: Request, res: Response): void => {
-
   const num: string = req.params.num as string;
-
+  
   const fibN: number = fibonacci(parseInt(num));
-
-  let result: string = fibonacci(${num}) is ${fibN};
+  
+  let result: string = `fibonacci(${num}) is ${fibN}`;
+  
   if (fibN < 0) {
-    result = fibonacci(${num}) is undefined;
+    result = `fibonacci(${num}) is undefined`;
   }
+  
   res.send(result);
 };
