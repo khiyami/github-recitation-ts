c9581ae5f2b563775f502889cdbf7356ac39c94e
Procos12 <64953008+Procos12@users.noreply.github.com>
2024-08-31 20:25:47 +0300

    any_attempt_1

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,17 +1,16 @@
 // Endpoint for querying the fibonacci numbers
-import fibonacci from "./fib";
-import { Request, Response } from "express";
 
-export default (req: Request, res: Response): void => {
-  const num: string = req.params.num as string;
-  
-  const fibN: number = fibonacci(parseInt(num));
-  
-  let result: string = `fibonacci(${num}) is ${fibN}`;
-  
+const fibonacci = require("./fib") as any;
+
+export default (req, res) => {
+  const { num } = req.params;
+
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
+
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
-  
+
   res.send(result);
 };
