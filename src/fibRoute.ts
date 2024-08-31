6a924792491cbc0190ca924458160a250e08c3f9
khiyami <68564739+khiyami@users.noreply.github.com>
2024-08-31 20:50:14 +0300

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,25 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { NextApiRequest, NextApiResponse } from 'next';
+import { fibonacci } from './fib';
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const num: string | undefined = req.query.num as string | undefined;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  if (!num || isNaN(parseInt(num))) {
+    res.status(400).send("Invalid input");
+    return;
+  }
+
+  const parsedNum = parseInt(num);
+  const fibN = fibonacci(parsedNum);
+
+  let result: string;
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = `fibonacci(${parsedNum}) is undefined`;
+  } else {
+    result = `fibonacci(${parsedNum}) is ${fibN}`;
   }
 
   res.send(result);
